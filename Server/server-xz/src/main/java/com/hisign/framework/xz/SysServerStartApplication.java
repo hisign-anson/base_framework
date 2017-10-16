76a6036a9370bd45fd95210b315d808224e708a1
huangyu0522 <384539643@qq.com>
2017-10-16 16:13:36 +0800

    fastjson冲突提交

---
+++ Server/server-xz/src/main/java/com/hisign/framework/xz/SysServerStartApplication.java
@@ -0,0 +1,18 @@
+package com.hisign;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+//@ComponentScan(basePackages = {"com.hisign"})
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SysServerStartApplication {
+
+    public static void main(String[] strs) {
+//        SpringApplication.run(applicationClass,strs);
+        new SpringApplicationBuilder(SysServerStartApplication.class).web(true).run(strs);
+    }
+}
