4295f0d5335a6b4cb988e54f6d2c0ff32a1171e4
xxhh <huihuanggungun@126.com>
2017-10-24 18:27:24 +0800

    update

--- Server/xingzhen-xz/xingzhen-xz-api/src/main/java/com/hisign/xingzhen/xz/api/entity/Ajgroup.java
+++ Server/xingzhen-xz/xingzhen-xz-api/src/main/java/com/hisign/xingzhen/xz/api/entity/Ajgroup.java
@@ -1,3 +1,4 @@
+
 package com.hisign.xingzhen.xz.api.entity;
 
 import java.io.Serializable;
@@ -9,44 +10,41 @@ import io.swagger.annotations.ApiModelProperty;
 
 /**
  * 《案件专案组关联》 实体
- * @author 刘玉兰
+ * @author 何建辉
  *
  */
 @ApiModel(value = "案件专案组关联")
 @Table(value="t_ajgroup")
 public class Ajgroup extends BaseModel implements Serializable {
-    private static final long serialVersionUID = 1L;
-
+	private static final long serialVersionUID = 1L;
 
 	@PK(value="ID")
 	@ApiModelProperty(value = "案件关联ID",hidden = true)
 	private String id; //案件关联ID
 
-    @Column(value="AJID")
-    @ApiModelProperty(value = "案件ID")
-    private String ajid; //案件ID
+	@Column(value="AJID")
+	@ApiModelProperty(value = "案件ID")
+	private String ajid; //案件ID
 
-    @Column(value="AJBH")
-    @ApiModelProperty(value = "案件编号")
-    private String ajbh; //案件编号
+	@Column(value="AJBH")
+	@ApiModelProperty(value = "案件编号")
+	private String ajbh; //案件编号
 
-    @Column(value="GROUPID")
-    @ApiModelProperty(value = "专案组ID")
-    private String groupid; //专案组ID
+	@Column(value="GROUPID")
+	@ApiModelProperty(value = "专案组ID")
+	private String groupid; //专案组ID
 
-    @Column(value="PGROUPID")
-    @ApiModelProperty(value = "父专案组ID")
-    private String pgroupid; //父专案组ID
-
+	@Column(value="CREATOR")
+	@ApiModelProperty(value = "创建人")
+	private String creator; //创建人
 
 	@Column(value="CREATETIME")
 	@ApiModelProperty(value = "创建时间",hidden = true)
 	private Date createtime; //创建时间
 
-    @Column(value="CREATETIME")
-    @ApiModelProperty(value = "创建时间")
-    private Date createtime; //创建时间
-
+	@Column(value="DEPARMENTCODE")
+	@ApiModelProperty(value = "创建人单位")
+	private String deparmentcode; //创建人单位
 
 	@Column(value="LASTUPDATETIME")
 	@ApiModelProperty(value = "修改时间",hidden = true)
@@ -55,15 +53,15 @@ public class Ajgroup extends BaseModel implements Serializable {
 	@Column(value="DELETEFLAG")
 	@ApiModelProperty(value = "删除标识",hidden = true)
 	private String deleteflag; //删除标识
-	
-    
+
+
 	/**
 	 *默认空构造函数
 	 */
 	public Ajgroup() {
 		super();
 	}
-	 
+
 	/**
 	 * @return ID 案件关联ID
 	 */
@@ -172,28 +170,28 @@ public class Ajgroup extends BaseModel implements Serializable {
 	public void setDeleteflag(String deleteflag){
 		this.deleteflag = deleteflag;
 	}
-	
+
 	public static String getTbName() {
 		return "t_ajgroup";
 	}
-	
+
 	@Override
 	public String toString(){
 		StringBuilder builder = new StringBuilder();
 		builder.append("Ajgroup [")
-		.append("id=").append(this.getId())
-		.append(",ajid=").append(this.getAjid())
-		.append(",ajbh=").append(this.getAjbh())
-		.append(",groupid=").append(this.getGroupid())
-		.append(",creator=").append(this.getCreator())
-		.append(",createtime=").append(this.getCreatetime())
-		.append(",deparmentcode=").append(this.getDeparmentcode())
-		.append(",lastupdatetime=").append(this.getLastupdatetime())
-		.append(",deleteflag=").append(this.getDeleteflag())
-		.append("]");
+				.append("id=").append(this.getId())
+				.append(",ajid=").append(this.getAjid())
+				.append(",ajbh=").append(this.getAjbh())
+				.append(",groupid=").append(this.getGroupid())
+				.append(",creator=").append(this.getCreator())
+				.append(",createtime=").append(this.getCreatetime())
+				.append(",deparmentcode=").append(this.getDeparmentcode())
+				.append(",lastupdatetime=").append(this.getLastupdatetime())
+				.append(",deleteflag=").append(this.getDeleteflag())
+				.append("]");
 		return builder.toString();
 	}
-	
+
 	public static enum AjgroupEnum{
 		id("ID","案件关联ID"),
 		ajid("AJID","案件ID"),
@@ -204,19 +202,19 @@ public class Ajgroup extends BaseModel implements Serializable {
 		deparmentcode("DEPARMENTCODE","创建人单位"),
 		lastupdatetime("LASTUPDATETIME","修改时间"),
 		deleteflag("DELETEFLAG","删除标识");
-		
+
 		private String fieldName;
 		private String remark;
-		
+
 		AjgroupEnum(String fieldName,String remark){
 			this.fieldName = fieldName;
 			this.remark = remark;
 		}
-		
+
 		public String get(){
 			return this.fieldName;
 		}
-		
+
 		public String getRemark(){
 			return this.remark;
 		}
