e68cdd7c3ce9eeede6ca37ad386d3995f86fd9ff
liuyulan <1025765021@qq.com>
2017-10-31 18:26:40 +0800

    rrr

--- Server/xingzhen-xz/xingzhen-xz-api/src/main/java/com/hisign/xingzhen/xz/api/param/TaskfkFileAddParam.java
+++ Server/xingzhen-xz/xingzhen-xz-api/src/main/java/com/hisign/xingzhen/xz/api/param/TaskfkFileAddParam.java
@@ -3,6 +3,7 @@ package com.hisign.xingzhen.xz.api.param;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import java.io.Serializable;
 
@@ -15,18 +16,23 @@ import java.io.Serializable;
 public class TaskfkFileAddParam implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+    @NotEmpty(message = "新文件名不能为空")
     @ApiModelProperty(value = "新文件名",required = true)
 	private String fileName; //新文件名
 
+    @NotEmpty(message = "原文件名不能为空")
     @ApiModelProperty(value = "原文件名",required = true)
 	private String fileOldName; //原文件名
 
+    @NotEmpty(message = "文件路径不能为空")
     @ApiModelProperty(value = "文件路径",required = true)
 	private String filePath; //文件路径
 
+    @NotEmpty(message = "文件类型不能为空")
     @ApiModelProperty(value = "文件类型",required = true)
 	private String fileType; //文件类型
 
+    @NotEmpty(message = "文件大小不能为空")
     @ApiModelProperty(value = "文件大小 单位K",required = true)
 	private Double fileSize; //文件大小 单位K
 
