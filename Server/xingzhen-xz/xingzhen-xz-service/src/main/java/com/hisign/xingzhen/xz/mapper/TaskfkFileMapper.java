6dbbef6182ff6232b21ca02e5c36561594dcf5d0
liyuegit <liyue@hisign.com.cn>
2017-11-07 21:16:05 +0800

    增加Repository注解

--- Server/xingzhen-xz/xingzhen-xz-service/src/main/java/com/hisign/xingzhen/xz/mapper/TaskfkFileMapper.java
+++ Server/xingzhen-xz/xingzhen-xz-service/src/main/java/com/hisign/xingzhen/xz/mapper/TaskfkFileMapper.java
@@ -4,12 +4,14 @@ package com.hisign.xingzhen.xz.mapper;
 import com.hisign.bfun.bif.BaseMapper;
 import com.hisign.xingzhen.xz.api.entity.TaskfkFile;
 import com.hisign.xingzhen.xz.api.model.TaskfkFileModel;
+import org.springframework.stereotype.Repository;
 
 /**
  * 《任务反馈文件表》 数据访问接口
  * @author 何建辉
  *
  */
+@Repository
 public interface TaskfkFileMapper extends BaseMapper<TaskfkFile,TaskfkFileModel,String> {
 
 }
