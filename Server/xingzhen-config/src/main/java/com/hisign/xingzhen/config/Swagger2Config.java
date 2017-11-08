894a68e612cd187d46dbdb94162d887e9b81630e
liuyulan <1025765021@qq.com>
2017-11-08 20:04:33 +0800

    111

--- Server/xingzhen-config/src/main/java/com/hisign/xingzhen/config/Swagger2Config.java
+++ Server/xingzhen-config/src/main/java/com/hisign/xingzhen/config/Swagger2Config.java
@@ -1,15 +1,9 @@
 package com.hisign.xingzhen.config;
 
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -17,9 +11,6 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -46,25 +37,5 @@ public class Swagger2Config {
                 .version("1.0")
                 .build();
     }
-
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setDateFormat("yyyy-MM-dd hh:mm:ss");
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-
-        List<MediaType> supportedMediaTypes = new ArrayList<MediaType>();
-
-        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-        fastConverter.setSupportedMediaTypes(supportedMediaTypes);
-        HttpMessageConverter<?> converter = fastConverter;
-
-        return new HttpMessageConverters(converter);
-    }
-
 
 }
